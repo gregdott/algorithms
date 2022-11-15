ad1f92b7ad11f8bd8597fac796457e665e5c9aeb
gregdott <dott.greg@gmail.com>
2022-11-15 11:03:29 +0000

    Update BinarySearch.java
    Cleaning & commenting

--- 25 Algos/src/BinarySearch.java
+++ 25 Algos/src/BinarySearch.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Author: Gregory Dott
  * 18-10-2022
  * 
@@ -14,15 +14,12 @@
  * 
  * Makes O(log n) comparisons (by taking advantage of the fact that the array is sorted) which is better than linear complexity (O(n))
  * 
- * There are 2 implementations of this algorithm below. A recursive one and a loopy (iterative) one.
+ * There are 2 implementations of this algorithm below. A recursive one and an iterative one.
  * Correct behaviour is not guaranteed (obviously) if the array provided is not in ascending order
  * That is why we call the checkArrayOrdering() function before doing anything and admonish the user for reprehensible behaviour
  * 
- * On the basic test array used below, the recursive implementation appears to be ~3x more efficient. Why?
- * Even after a bit of clean up in the iterative version, the recursive solution still outperforms it by around 3x... Very interesting.
- * Lol, the time testing is not foolproof. Switching which one is called first around, the one that is called first takes ~3x longer.
- * So there must be some memory operations (possibly to do with the array?) that take place when the first one is called but do not
- * need to happen when the second one is called. Need a more robust way of testing execution times...
+ * A value of -1 indicates the item being searched for is not in the array provided.
+ * 
  */
 
 public class BinarySearch {
@@ -31,24 +28,11 @@ public class BinarySearch {
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
         if (checkArrayAscending(arr)) {
-            long startTime1 = System.nanoTime();
-            System.out.println(Integer.toString(searchLoopy(arr, 5)));
-            long stopTime1 = System.nanoTime();
-            long timeTaken1 = stopTime1 - startTime1;
-
-            System.out.println("TIME TAKEN (loop): " + Long.toString(timeTaken1));
-
-            long startTime2 = System.nanoTime();
-            System.out.println(Integer.toString(searchRecursive(arr, 5, 0, arr.length - 1)));
-            long stopTime2 = System.nanoTime();
-            long timeTaken2 = stopTime2 - startTime2;
-
-            System.out.println("TIME TAKEN (recursion): " + Long.toString(timeTaken2));
-            
+            Pr.x("Recursive search: " + Integer.toString(searchRecursive(arr, 5, 0, arr.length - 1)), "=");
+            Pr.x("Iterative search: " + Integer.toString(searchIterative(arr, 5)), "=");
         } else {
             System.out.println("The Binary Search algorithm requires the provided array is arranged in ascending order. Please correct this!");
         }
-        
     }
 
     /**
@@ -78,13 +62,13 @@ public class BinarySearch {
     }
 
     /**
-     * searchLoopy: implementation of the Binary Search algorithm, implemented using a while loop (and not recursion)
+     * searchIterative: implementation of the Binary Search algorithm, implemented using a while loop (and not recursion)
      * 
      * @param arr array of integers which should be sorted in ascending order
      * @param toFind value of element to search for
      * @return -1 if toFind is not in arr, otherwise it returns the index of the first instance found
      */
-    public static int searchLoopy(int[] arr, int toFind) {
+    public static int searchIterative(int[] arr, int toFind) {
         
         int middle, start = 0, end = arr.length - 1;
         middle = start + (end - start)/2;
